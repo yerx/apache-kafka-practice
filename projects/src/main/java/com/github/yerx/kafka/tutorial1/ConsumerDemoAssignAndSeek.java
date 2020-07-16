0a9f129647487901a869d4e22e50098957a22b87
yerx <yer.x.xiong@gmail.com>
2020-07-16 12:54:30 -0400

    completed consumer assign and seek demo

---
+++ projects/src/main/java/com/github/yerx/kafka/tutorial1/ConsumerDemoAssignAndSeek.java
@@ -0,0 +1,67 @@
+package com.github.yerx.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoAssignAndSeek {
+
+    public static void main(String[] args) {
+
+     Logger logger = LoggerFactory.getLogger(ConsumerDemoAssignAndSeek.class.getName());
+
+     String boostrapServers = "127.0.0.1:9092";
+     String topic = "first_topic";
+
+     // create consumer configs
+     Properties properties = new Properties();
+     properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+     properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+     properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+     properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+     // create consumer
+     KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+     // assign and seek are mostly used to replay data or fetch a specific message
+
+     // assign
+     TopicPartition partitionToReadFrom = new TopicPartition(topic, partition:0);
+     long offsetToReadFrom = 15L;
+     consumer.assign(Arrays.asList(partitionToReadFrom));
+
+     // seek
+     consumer.seek(partitionToReadFrom, offsetToReadFrom);
+
+     int numberOfMessagesToRead = 5;
+     boolean keepOnReading = true;
+     int numberOfMessagesReadSoFar = 0;
+
+     // poll for new data
+     while(true) {
+         ConsumerRecords<String, String> records =
+                 consumer.poll(Duration.ofMillis(100));
+
+         for (ConsumerRecord<String, String> record : records) {
+             numberOfMessagesReadSoFar += 1;
+             logger.info("Key: " + record.key() + ", Value: " + record.value());
+             logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+             if (numberOfMessagesReadSoFar >= numberOfMessagesToRead) {
+                 keepOnReading = false;
+                 break;
+             }
+         }
+     }
+     logger.info("exiting the application");
+ }
+}
